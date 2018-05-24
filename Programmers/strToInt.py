b0659a22cd89c45037a2d3974a4752dc3822f464
ieon <kimiron518@naver.com>
2018-05-24 09:51:00 +0900

    Add fibonacci and strToInt

---
+++ Programmers/strToInt.py
@@ -0,0 +1,12 @@
+#
+# From : https://programmers.co.kr/learn/challenge_codes/111
+#
+
+def strToInt(str):
+    result = int(str)
+    #함수를 완성하세요
+    return result
+
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(strToInt("-1234"));
