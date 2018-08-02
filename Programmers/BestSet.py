fc4f146f0751c1192effe10db550265a54bface4
ieon <kimiron518@gmail.com>
2018-08-02 15:23:40 +0900

    resolve best set

--- Programmers/BestSet.py
+++ Programmers/BestSet.py
@@ -4,13 +4,35 @@
 from math import ceil
 
 def solution(n,s):
+    if n>s:
+        return [-1]
     answer = []
-
-    # s/2
-
-    return answer
+    mid = ceil(s/n)
+    answer.append(mid)
+    if n == 1:
+        return answer
+    else:
+        answer.extend(solution(n-1, s-mid))
+    return sorted(answer)
 
 # [4,5]
 print(solution(2,9))
+print(solution(3,10))
+print(solution(4,14))
+print('-----------------------')
 
-print(ceil(9/2))
+# 다른 사람 풀이
+def bestSet(n, s):
+    if n > s:
+        return [-1]
+    portion, remainder = divmod(s, n)
+    print("portion:",portion, "reminder:",remainder)
+    li = [portion] * n
+    for i in range(remainder):
+        li[i] += 1
+
+    return sorted(li)
+
+print(bestSet(2,9))
+print(bestSet(3,10))
+print(bestSet(4,14))
