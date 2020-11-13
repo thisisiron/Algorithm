f59cb446a5552cb4f12f879053bcbac773c4849e
Eon Kim <kimiron518@gmail.com>
2020-11-13 17:38:35 +0900

    add code

---
+++ HackerRank/Sorting/Comparator.py
@@ -0,0 +1,23 @@
+# link: https://www.hackerrank.com/challenges/ctci-comparator-sorting/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=sorting
+
+
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+        
+    def __repr__(self):
+        return repr(self.name + " " + self.score)
+        
+    def comparator(a, b):
+        if a.score > b.score:
+            return -1
+        elif a.score < b.score:
+            return 1
+        else:
+            if a.name < b.name:
+                return -1
+            elif a.name > b.name:
+                return 1
+            else:
+                return 0
