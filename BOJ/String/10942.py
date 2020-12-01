407e9bd1cbe9980a657f5f1acd6cf55cc81581b7
Eon Kim <kimiron518@gmail.com>
2020-12-01 23:52:48 +0900

    add code and update code

--- BOJ/String/10942.py
+++ BOJ/String/10942.py
@@ -13,7 +13,7 @@ for i in range(N - 1):
     if x_list[i] == x_list[i + 1]:
         dp[i][i + 1] = 1
 
-for i in range(1, N):
+for i in range(2, N):
     for j in range(2, i + 1):
         if x_list[i] == x_list[i - j] and dp[i - j + 1][i - 1]:
             dp[i - j][i] = 1
