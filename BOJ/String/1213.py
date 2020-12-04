85fcba6fe629fcf6bd9a160701ad9e8cc1174440
Eon Kim <kimiron518@gmail.com>
2020-12-04 22:57:06 +0900

    add code

---
+++ BOJ/String/1213.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+name = input().rstrip()
+name_cnt = [0 for _ in range(26)]
+
+for char in name:
+    name_cnt[ord(char) - 65] += 1
+
+odd = 0
+odd_alpha =''
+alpha = ''
+
+for i in range(26):
+    if name_cnt[i] % 2 == 1:
+        odd += 1
+        odd_alpha += chr(i + 65)
+    alpha += chr(i + 65) * (name_cnt[i] // 2)
+
+if odd > 1:
+    print("I'm Sorry Hansoo")
+else:
+    print(alpha + odd_alpha + alpha[::-1])
