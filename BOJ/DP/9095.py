8d0ef934cbfd9307139307b0b82aa46b6bf2048e
Eon Kim <kimiron518@gmail.com>
2021-01-19 20:16:40 +0900

    add codes

---
+++ BOJ/DP/9095.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+
+T = int(input())
+dp = [0] * 12
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for _ in range(T):
+    n = int(input())
+    for i in range(4, n + 1):
+        dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1]
+    print(dp[n])
