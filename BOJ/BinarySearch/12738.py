bafba3115737a5d981a6094ca5bd977ee4b227fb
Eon Kim <kimiron518@gmail.com>
2021-02-24 23:15:36 +0900

    add code

---
+++ BOJ/BinarySearch/12738.py
@@ -0,0 +1,17 @@
+import sys
+import bisect
+input = sys.stdin.readline
+
+
+N = int(input())
+arr = list(map(int, input().split()))
+
+dp = [arr[0]]
+for i in range(N):
+    if arr[i] > dp[-1]:
+        dp.append(arr[i])
+    else:
+        idx = bisect.bisect_left(dp, arr[i])
+        dp[idx] = arr[i]
+
+print(len(dp))
