39f5178dc569ca4e8a93bd2360125bf04412d570
Eon Kim <kimiron518@gmail.com>
2021-05-16 22:47:43 +0900

    add code

---
+++ BOJ/Bruteforcing/15686.py
@@ -0,0 +1,30 @@
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+
+N, M = map(int, input().split())
+
+home = []
+chicken = []
+
+for r in range(N):
+    for c, p in enumerate(map(int, input().split())):
+        if p == 1:
+            home.append((r, c))
+        elif p == 2:
+            chicken.append((r, c))
+
+combs = combinations(chicken, M)
+
+mn = float('inf')
+for comb in combs:
+    dist = 0
+    for h in home:
+        dist += min([abs(h[0] - ch[0]) + abs(h[1] - ch[1]) for ch in comb])
+        if mn <= dist:
+            break
+
+    if dist < mn:
+        mn = dist
+print(mn)
