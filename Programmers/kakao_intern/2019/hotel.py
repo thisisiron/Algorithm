ad2f1f6d72e531e30ee244e0440eb7018289fab9
thisisiron <kimiron518@gmail.com>
2021-07-02 22:30:02 +0900

    add hotel

--- Programmers/kakao_intern/2019/hotel.py
+++ Programmers/kakao_intern/2019/hotel.py
@@ -1,6 +1,24 @@
-def solution(k: int, room_number: list) -> list:
+from collections import defaultdict
+import sys
+sys.setrecursionlimit(10**9)
+
+
+def solution(k, room_number):
+    room = defaultdict(lambda: 0)
     answer = []
+    for n in room_number:
+        check = find(room, n)
+        answer.append(check)
+        room[check] = check + 1
     return answer
+
+
+def find(room, x):
+    if room[x] == 0:
+        return x
+    else:
+        room[x] = find(room, room[x])
+        return room[x]
 
 
 if __name__ == '__main__':
