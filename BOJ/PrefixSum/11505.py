32701c87c72ff08ed3ca7fb5f9f75120ea0df0f5
Eon Kim <kimiron518@gmail.com>
2021-08-12 12:18:32 +0900

    update 11505

--- BOJ/PrefixSum/11505.py
+++ BOJ/PrefixSum/11505.py
@@ -5,58 +5,51 @@ input = sys.stdin.readline
 def init(start, end, cur):
     if start == end:
         tree[cur] = arr[start]
-        return tree[cur]
-
-    mid = (start + end) // 2
-
-    tree[cur] = init(start, mid, cur * 2) * init(mid + 1, end, cur * 2 + 1)
+    else:
+        mid = (start + end)//2
+        tree[cur] = (init(start, mid, cur * 2) * init(mid + 1, end, cur * 2 + 1)) % 1000000007
     return tree[cur]
 
 
 def update(start, end, cur, idx, val):
-    if idx < start or idx > end:
+    if idx < start or end < idx:
         return
 
-    tree[cur] *= val
-
     if start == end:
+        tree[cur] = val
         return
 
     mid = (start + end) // 2
 
     update(start, mid, cur * 2, idx, val)
     update(mid + 1, end, cur * 2 + 1, idx, val)
+    tree[cur] = (tree[cur * 2] * tree[cur * 2 + 1]) % 1000000007
 
 
 def mul_interval(start, end, cur, left, right):
-    if left > end or right < start:
+    if right < start or end < left:
         return 1
 
     if left <= start and end <= right:
         return tree[cur]
 
     mid = (start + end) // 2
-    return mul_interval(start, mid, cur * 2, left, right) * mul_interval(mid + 1, end, cur * 2 + 1, left, right)
+    val = mul_interval(start, mid, cur * 2, left, right) * mul_interval(mid + 1, end, cur * 2 + 1, left, right)
+    return val % 1000000007
 
 
-N, M, K  = map(int, input().split())
+N, M, K = map(int, input().split())
 
-arr = []
-for _ in range(N):
-    arr.append(int(input()))
+arr = [int(input()) for i in range(N)]
 
-tree = [1] * 4 * N
+tree = [0] * (4 * N)
 init(0, N - 1, 1)
 
-for i in range(M + K):
+for _ in range(M + K):
     a, b, c = map(int, input().split())
 
     if a == 1:
-        if arr[b - 1] != 0:
-            update(0, N - 1, 1, b - 1, c / arr[b - 1])
-            arr[b - 1] = c
-        elif arr[b - 1] == 0:
-            arr[b - 1] = c
-            init(0, N - 1, 1)
+        update(0, N - 1, 1, b - 1, c)
+        arr[b - 1] = c
     elif a == 2:
-        print(int(mul_interval(0, N - 1, 1, b - 1, c - 1)))
+        print(mul_interval(0, N - 1, 1, b - 1, c - 1))
